413b517cbf35a62fd0561184640e2a3acf208df5
Deepika R <deepikarajagopal8@gmail.com>
2026-09-17 18:16:56 +0530

    Time: 4 ms (83.74%), Space: 46.4 MB (33.7%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        char[] a=s.toCharArray();
+        char[] b=t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if(Arrays.equals(a,b)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        
+    }
+}
